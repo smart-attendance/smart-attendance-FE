04c5d3238c4c0017bc41560d15a4b3f84ae56b40
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2022-03-24 09:53:40 +0700

    feat: login

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Routers from './routes';
+
+function App() {
+  return (
+    <Routers />
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    fontFamily: {
+      primary: ['OpenSans', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        brown: {
+          primary: '#DB9069',
+          secondary: '#DBA569'
+        }
+      },
+    },
+  },
+  plugins: [],
+}
